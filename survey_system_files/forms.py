e2cee2b1f413597a267cd86057159cc9479f1457
TheJokersThief <esincork@gmail.com>
2014-12-16 22:16:37 +0000

    Refactoring the forms

--- survey_system_files/forms.py
+++ survey_system_files/forms.py
@@ -18,24 +18,36 @@ class SurveyForm( forms.Form ):
 				self.fields['%s' % question.id] = forms.CharField(label= question.text, required = required, widget = forms.Textarea() )
 
 			elif question.input_type == 'radio':
-				CHOICES=[]
-
-				choices_delimited = question.choices.split( ',' )
-
-				for choice in choices_delimited:
-					CHOICES.append((choice, choice))
+				CHOICES = self.get_choices( question.choices )
 
 				self.fields['%s' % question.id] = forms.ChoiceField(label= question.text, choices=CHOICES, widget=forms.RadioSelect(), required = required)
 
 			elif question.input_type == 'select':
-				CHOICES=[]
+				CHOICES = self.get_choices( question.choices )
 
-				choices_delimited = question.choices.split( ',' )
-
-				for choice in choices_delimited:
-					CHOICES.append((choice, choice))
-
-				self.fields['%s' % question.id] = forms.ChoiceField(label= question.text, choices = CHOICES)
+				self.fields['%s' % question.id] = forms.ChoiceField(label= question.text, choices = CHOICES,  widget=forms.Select(attrs={'required': required}))
 
 			elif question.input_type == 'checkbox':
-				self.fields['%s' % question.id] = forms.BooleanField(label= question.text, required = required)
+				CHOICES = self.get_choices( question.choices )
+
+				self.fields['%s' % question.id] = forms.MultipleChoiceField(label= question.text, choices = CHOICES, widget=forms.CheckboxSelectMultiple(), required = required)
+
+			elif question.input_type == 'order':
+				CHOICES = self.get_choices( question.choices )
+
+				self.fields['%s' % question.id] = forms.MultipleChoiceField(label= question.text, 
+																	choices=CHOICES, 
+																	widget=forms.CheckboxSelectMultiple(attrs={'class': "sortable"}),
+																	required = required,
+																	)
+
+	def get_choices( self, choices ):
+		CHOICES=[]
+
+		choices_delimited = choices.split( ',' )
+
+		for choice in choices_delimited:
+			CHOICES.append((choice, choice))
+
+		return CHOICES
+
